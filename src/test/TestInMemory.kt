8edcbaf9bb7e03d1b73033e8caf6a7d268066286
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-09 23:13:39 +0200

    fix checkDeleteTransction

--- src/test/TestInMemory.kt
+++ src/test/TestInMemory.kt
@@ -1,28 +1,32 @@
 package test
 
-package testing
 import Transaction
-import java.util.Date
+import java.time.LocalDate
 
 fun main() {
     checkDeleteTransction()
 }
 
-fun checkDeleteTransction(){
-    var myTransaction =mutableListOf(
-        Transaction(Date(2030,12,20)))
+fun checkDeleteTransction() {
+    var myTransaction = mutableListOf(
+        Transaction(amount = 100.0, category = "Travel", date = LocalDate(2030, 12, 20))
+    )
 
-    val user1= User(mutableListOf(
-        Transaction(Date(2000,11,20)),
-        Transaction(Date(2030,12,20))))
+    val user1 = User(
+        mutableListOf(
+            Transaction(amount = 100.0, category = "Travel", LocalDate(2000, 11, 20)),
+            Transaction(amount = 100.0, category = "Travel", date = LocalDate(2030, 12, 20))
+        )
+    )
 
-    check(case="transaction not DELETED ",reterned_Result=user1.transactions,correctResult=myTransaction)
-    user1.deleteTransaction(Transaction(Date(2000,11,20)))
-    check(case="transaction DELETED ",reterned_Result=user1.transactions,correctResult=myTransaction)
+    user1.deleteTransaction(Transaction(amount = 100.0, category = "Travel", LocalDate(2000, 11, 20)))
+    checkTransaction(case = "transaction DELETED ", reterned_Result = user1.transactions, correctResult = myTransaction)
 
 }
-fun check(case :String, reterned_Result: MutableList<Transaction>, correctResult:MutableList<Transaction>){
-    if (reterned_Result ==correctResult){
+
+
+fun checkTransaction(case: String, reterned_Result: MutableList<Transaction>, correctResult: MutableList<Transaction>) {
+    if (reterned_Result == correctResult) {
         println("success_ ")
     }else{
         println("failed_ ")
