128a7617b83cd8cb14341c90b74148b386e10c59
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-09 23:10:59 +0200

    Revert "fix deleteTransaction Test"
    This reverts commit ef333833f65869c893418436cb2df3ae44a1d106.

--- src/test/TestInMemory.kt
+++ src/test/TestInMemory.kt
@@ -1,42 +1,37 @@
 package test
 
+package testing
 import Transaction
-import java.time.LocalDate
+import java.util.Date
 
 fun main() {
     checkDeleteTransction()
 }
 
-fun checkDeleteTransction() {
-    var myTransaction = mutableListOf(
-        Transaction(amount = 100.0, category = "Travel", date = LocalDate(2030, 12, 20))
-    )
+fun checkDeleteTransction(){
+    var myTransaction =mutableListOf(
+        Transaction(Date(2030,12,20)))
 
-    val user1 = User(
-        mutableListOf(
-            Transaction(amount = 100.0, category = "Travel", LocalDate(2000, 11, 20)),
-            Transaction(amount = 100.0, category = "Travel", date = LocalDate(2030, 12, 20))
-        )
-    )
+    val user1= User(mutableListOf(
+        Transaction(Date(2000,11,20)),
+        Transaction(Date(2030,12,20))))
 
-//    check(case="transaction not DELETED ",reterned_Result=user1.transactions,correctResult=myTransaction)
-    user1.deleteTransaction(Transaction(amount = 100.0, category = "Travel", LocalDate(2000, 11, 20)))
-    check(case = "transaction DELETED ", reterned_Result = user1.transactions, correctResult = myTransaction)
+    check(case="transaction not DELETED ",reterned_Result=user1.transactions,correctResult=myTransaction)
+    user1.deleteTransaction(Transaction(Date(2000,11,20)))
+    check(case="transaction DELETED ",reterned_Result=user1.transactions,correctResult=myTransaction)
 
 }
-
-
-fun check(case: String, reterned_Result: MutableList<Transaction>, correctResult: MutableList<Transaction>) {
-    if (reterned_Result == correctResult) {
+fun check(case :String, reterned_Result: MutableList<Transaction>, correctResult:MutableList<Transaction>){
+    if (reterned_Result ==correctResult){
         println("success_ ")
-    } else {
+    }else{
         println("failed_ ")
     }
 }
 
 class User(val transactions: MutableList<Transaction>) {
 
-    fun deleteTransaction(transaction: Transaction) {
+    fun deleteTransaction( transaction: Transaction) {
 
         transactions.remove(transaction)
     }
