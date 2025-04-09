32f3a51f6a9c25ef17b8db0721c3b70254cc3d0a
safa thaaer <safathaaer@gmail.com>
2025-04-09 08:11:53 -0700

    Add transaction model, validation, and test cases

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
+import java.time.LocalDate
+// Run transaction validation checks
 fun main() {
+    val validTransaction = Transaction(
+        amount = 120.0,
+        category = "Salary",
+        date = LocalDate.now()
+    )
+
+    val invalidTransaction = Transaction(
+        amount = -40.0,
+        category = "",
+        date = LocalDate.now().plusDays(3)
+    )
+
+    println("Valid Transaction:$validTransaction ")
+
+
+
+    println("Invalid Transaction:$invalidTransaction")
+
 
 }
