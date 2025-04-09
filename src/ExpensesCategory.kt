3bf70a6a98ff17bc859bceb469cbe79430ede0cf
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-04-09 18:01:47 +0200

    add expenses categories

--- src/ExpensesCategory.kt
+++ src/ExpensesCategory.kt
@@ -1,4 +1,14 @@
 enum class ExpensesCategory (
     val transactionsCategoryType: TransactionsCategoryType = TransactionsCategoryType.EXPENSES
 ){
+    SHOPPING,
+    TRAVEL,
+    FOOD,
+    RENT,
+    GIFTS,
+    BILLS,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    PERSONAL_NEEDS,
+    OTHER,
 }
