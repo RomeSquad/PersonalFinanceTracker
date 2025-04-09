54e347405534d060b672239732e9339c1ffba7a4
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-09 22:43:58 +0200

    feature/balance-Report: balance-Report impl

---
+++ src/report/BalanceReport.kt
@@ -0,0 +1,27 @@
+package report
+
+import database.TransactionsDB
+import java.text.NumberFormat
+
+class BalanceReport (private val transactionsDB: TransactionsDB): Report{
+    override fun generateReport(): String {
+        val totalIncome = transactionsDB.getTotalIncome()
+        val formattedIncome = NumberFormat.getCurrencyInstance().format(totalIncome)
+        val totalExpense = transactionsDB.getTotalExpenses()
+        val formattedExpense = NumberFormat.getCurrencyInstance().format(totalExpense)
+        val netBalance = transactionsDB.getTotalIncome()
+        val formattedBalance = NumberFormat.getCurrencyInstance().format(netBalance)
+
+        return """
+            |=================================
+            |          BALANCE REPORT         
+            |=================================
+            |Total Income:  $${formattedIncome}
+            |Total Expense: $${formattedExpense}
+            |--------------------------------
+            |Net Balance:   $${formattedBalance}
+            |=================================
+        """.trimMargin()
+    }
+
+}
