d266b3c15c78f3b3ab97e335febed3c966adaa8d
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-04-10 10:34:05 +0200

    create property category name and init data of all category name

--- src/ExpensesCategory.kt
+++ src/ExpensesCategory.kt
@@ -1,14 +1,15 @@
 enum class ExpensesCategory (
-    val transactionsCategoryType: TransactionsCategoryType = TransactionsCategoryType.EXPENSES
+    val transactionsCategoryType: TransactionsCategoryType = TransactionsCategoryType.EXPENSES ,
+    val categoryName : String
 ){
-    SHOPPING,
-    TRAVEL,
-    FOOD,
-    RENT,
-    GIFTS,
-    BILLS,
-    TRANSPORTATION,
-    ENTERTAINMENT,
-    PERSONAL_NEEDS,
-    OTHER,
+    SHOPPING(categoryName = "shopping"),
+    TRAVEL(categoryName = "travel"),
+    FOOD(categoryName = "food"),
+    RENT(categoryName = "rent"),
+    GIFTS(categoryName = "gifts"),
+    BILLS(categoryName = "bills"),
+    TRANSPORTATION(categoryName = "transportation"),
+    ENTERTAINMENT(categoryName = "entertainment"),
+    PERSONAL_NEEDS(categoryName = "personal needs"),
+    OTHER(categoryName = "other"),
 }
--- src/IncomeCategory.kt
+++ src/IncomeCategory.kt
@@ -1,10 +1,11 @@
 enum class IncomeCategory(
-    val transactionsCategoryType: TransactionsCategoryType = TransactionsCategoryType.INCOME
+    val transactionsCategoryType: TransactionsCategoryType = TransactionsCategoryType.INCOME,
+    val categoryName : String
 ) {
-    SALARY,
-    FREELANCE,
-    RENT,
-    GIFT,
-    BUSINESS,
-    OTHER,
+    SALARY(categoryName = "salary"),
+    FREELANCE(categoryName = "freelance"),
+    RENT(categoryName = "rent"),
+    GIFT(categoryName = "gift"),
+    BUSINESS(categoryName = "business"),
+    OTHER(categoryName = "other"),
 }
