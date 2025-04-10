8093a27ae35ad395992d20a8e79f5dcab8e98663
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-10 14:16:46 +0200

    refactor transaction storage

--- src/database/TransactionsDB.kt
+++
@@ -1,9 +0,0 @@
-package database
-
-import Transaction
-//
-interface TransactionsDB {
-    fun getTotalBalance(): Double
-    fun getTotalIncome(): Double
-    fun getTotalExpenses(): Double
-}
---
+++ src/database/TransactionsManager.kt
@@ -0,0 +1,15 @@
+package database
+
+import entity.Transaction
+
+interface TransactionsManager {
+    fun addTransaction(transaction: Transaction)
+    fun deleteTransaction(transaction: Transaction)
+    fun getTransaction(transactionId: String) : Transaction?
+    fun getAllTransactions(): List<Transaction>
+    fun editTransaction(transaction: Transaction)
+
+    fun getTotalBalance(): Double
+    fun getTotalIncome(): Double
+    fun getTotalExpenses(): Double
+}
