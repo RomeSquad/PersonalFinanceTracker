5a349a9d595591c82625d14da5eb4bacdb21fb60
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-04-11 13:28:05 +0200

    refactor test case implementation

--- src/test/managerActionTest/editTransactionTest.kt
+++ src/test/managerActionTest/editTransactionTest.kt
@@ -1,200 +1,73 @@
 package test.managerActionTest
 
-import Transaction
-import TransactionType
-import java.time.LocalDate
+import entity.Categories
 
 fun editTransactionTestCases() {
-    val transactionMangerActionsTest = TransactionMangerActionsTest()
-
     //region Valid Cases for Each Field
     test.check(
-        name = "when all attributes are valid should return true",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
+        name = "when enter a valid and in range number return true",
+        expectedResult = Validator.isValidTransactionType("1"),
         correctResult = true
     )
     test.check(
-        name = "when amount is valid should return true",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = true
+        name = "when enter an empty string return false",
+        expectedResult = Validator.isValidTransactionType(""),
+        correctResult = false
     )
     test.check(
-        name = "when category is valid should return true",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = true
+        name = "when enter a negative number return false",
+        expectedResult = Validator.isValidTransactionType("-1"),
+        correctResult = false
     )
     test.check(
-        name = "when date is valid should return true",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = true
+        name = "when enter an out of range number return false",
+        expectedResult = Validator.isValidTransactionType("9"),
+        correctResult = false
+    )
+    test.check(
+        name = "when enter a character return false",
+        expectedResult = Validator.isValidTransactionType("a"),
+        correctResult = false
     )
     //endregion
 
     //region Invalid Cases for amount Field
     test.check(
-        name = "when amount is invalid should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = -100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
+        name = "when enter a decimal positive number return true",
+        expectedResult = Validator.isValidAmount("1400.5"),
+        correctResult = true
+    )
+    test.check(
+        name = "when enter a negative number return false",
+        expectedResult = Validator.isValidAmount("-1500"),
         correctResult = false
     )
     test.check(
-        name = "when editing transaction with zero amount should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 0.0,
-                category = "Food",
-                type = TransactionType.EXPENSE
-            )
-        ),
+        name = "when enter a character return false",
+        expectedResult = Validator.isValidAmount("a"),
+        correctResult = false
+    )
+    test.check(
+        name = "when enter an empty string return false",
+        expectedResult = Validator.isValidAmount(""),
         correctResult = false
     )
     //endregion
 
     //region Invalid Cases for category Field
     test.check(
-        name = "when editing transaction with empty category should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 150.0,
-                category = "",
-                type = TransactionType.EXPENSE
-            )
-        ),
+        name = "when enter an input with just characters return true",
+        expectedResult = Validator.isValidCategory(Categories.FOOD.name),
+        correctResult = true
+    )
+    test.check(
+        name = "when enter just numbers return false",
+        expectedResult = Validator.isValidCategory("45201"),
         correctResult = false
     )
     test.check(
-        name = "when category be maximum length should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "A".repeat(51), // Assuming 50 is max length
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when category contains special characters should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Food@123",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when category is blank should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "   ",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when category contains only numbers should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "1234567890",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when category contains leading and trailing spaces should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "   Food   ",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    //endregion
-
-    //region Invalid Cases for type Field
-    test.check(
-        name = "when type is EXPENSE with negative amount should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = -100.0,
-                category = "Valid",
-                type = TransactionType.EXPENSE
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when type is INCOME with negative amount should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = -100.0,
-                category = "Valid",
-                type = TransactionType.INCOME
-            )
-        ),
-        correctResult = false
-    )
-    test.check(
-        name = "when type is null should return false",
-        expectedResult = transactionMangerActionsTest.editTransaction(
-            Transaction(
-                date = LocalDate.now(),
-                amount = 100.0,
-                category = "Valid",
-                type = null
-            )
-        ),
+        name = "when enter an empty string return false",
+        expectedResult = Validator.isValidCategory(""),
         correctResult = false
     )
     //endregion
