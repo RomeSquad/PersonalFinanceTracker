47ed08e96fb120c03a612fd5a6d138e8bbe20637
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-12 01:52:49 +0200

    test delete in json file

---
+++ src/test/TestInJsonFile.kt
@@ -0,0 +1,76 @@
+package test
+
+
+import database.TransactionsInJsonSerialization
+import database.TransactionsInMemory
+import entity.Category
+import entity.ExpensesCategories
+import entity.Transaction
+import entity.TransactionsType
+import java.time.LocalDate
+
+
+fun main() {
+    checkDeleteTransactionWithId()
+    checkDeleteTransactionWithObject()
+}
+
+fun checkDeleteTransactionWithId() {
+    val transactionToDelete = newMockTransaction()
+
+
+    val inJsonFile = TransactionsInJsonSerialization()
+
+    inJsonFile.addTransaction(newMockTransaction())
+    inJsonFile.addTransaction(transactionToDelete)
+    inJsonFile.deleteTransaction(transactionToDelete.id)
+
+    if (inJsonFile.getAllTransactions().contains(transactionToDelete)) {
+        println("failed_ transaction with id = $transactionToDelete wasn't deleted")
+    } else {
+        println("success_ transaction is deleted with id")
+    }
+}
+
+fun checkDeleteTransactionWithObject() {
+
+    val mockTransactionToDelete = newMockTransaction()
+    val mockTransactionToKeep = newMockTransaction()
+
+    val myTransaction = mutableListOf(
+        mockTransactionToKeep
+    )
+
+    val inMemory = TransactionsInMemory()
+    inMemory.addTransaction(
+        mockTransactionToDelete
+    )
+
+    inMemory.addTransaction(
+        mockTransactionToKeep
+    )
+
+    inMemory.deleteTransaction(
+        mockTransactionToDelete
+    )
+
+    println(inMemory.transactions)
+    println(myTransaction)
+    println(
+        test.check(
+            name = "transaction DELETED with transaction object",
+            expectedResult = inMemory.transactions,
+            correctResult = myTransaction
+        )
+    )
+}
+
+
+fun newMockTransaction(): Transaction {
+    return Transaction(
+        amount = Math.random(),
+        category = Category.Expenses(ExpensesCategories.TRAVEL),
+        transactionsType = TransactionsType.EXPENSES,
+        date = LocalDate.now()
+    )
+}
