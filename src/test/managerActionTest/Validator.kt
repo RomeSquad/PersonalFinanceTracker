8d4a874afd5ce15490e62648ca16b0eb5207e842
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-04-11 13:27:21 +0200

    make validator object for test case check

---
+++ src/test/managerActionTest/Validator.kt
@@ -0,0 +1,25 @@
+package test.managerActionTest
+
+import entity.TransactionsType
+
+
+object Validator {
+
+    fun isValidTransactionType(input: String): Boolean {
+        val index = input.toIntOrNull() ?: -1
+        return TransactionsType.entries.getOrNull(index - 1) != null
+    }
+
+    fun isValidCategory(input: String): Boolean {
+        if (input.isEmpty() || input.isBlank()) return false
+        if (input.all { it.isDigit() }) return false
+        return true
+    }
+
+    fun isValidAmount(input: String): Boolean {
+        val value = input.toDoubleOrNull() ?: return false
+        if (value < 0) return false
+        return true
+    }
+
+}
